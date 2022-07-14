f316ce5f67bcc0d3c600a6248b080e8d733eb719
reginasar <50836927+reginasar@users.noreply.github.com>
2022-07-14 12:28:32 +0100

    Update example.py

--- example.py
+++ example.py
@@ -7,18 +7,19 @@ re_kpc = 1.37177
 basePath = 'path/to/TNG/Illustris/data/'
 
 ########## Step 1 ###########
-mk_particle_files(subhalo, snap, basePath, ex=[1,0,0], FOV=19, overwrite=True)
+sifu.mk_particle_files(subhalo, snap, basePath, ex=[1,0,0], FOV=19, overwrite=True)
 
 ########## Step 2 ###########
 #For the next step template gas file must be dowloaded separately and saved in libs directory templete_gas.fits.gz
-
-mk_mock_RSS(snap, subhalo, view=0, sp_samp=1., template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz',\
-                template_SSP='libs/MaStar_CB19.slog_1_5.fits.gz', template_gas='libs/templete_gas.fits.gz', fib_n=7)
-# mk_mock_RSS outputs
+# mk_mock_RSS outputs a RSS file called
 rss_file = 'ilust-'+str(snap)+'-'+str(subahlo)+'-0-127.cube_RSS.fits.gz'
 
+sifu.mk_mock_RSS(snap, subhalo, view=0, sp_samp=1., template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz',\
+                template_SSP='libs/MaStar_CB19.slog_1_5.fits.gz', template_gas='libs/templete_gas.fits.gz', fib_n=7)
+
+
 ########## Step 3 ###########
-regrid(rss_file, template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz', \
+sifu.regrid(rss_file, template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz', \
             n_fib=7, thet=0.0, R_eff=re_kpc, include_gas=False)
 
 ########## Step 4 ###########
