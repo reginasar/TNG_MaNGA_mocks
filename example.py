a908ff95b4ae07e826b1fb38eb094fbdeb57b1e5
reginasar <50836927+reginasar@users.noreply.github.com>
2022-07-26 12:56:23 +0100

    Update example.py

--- example.py
+++ example.py
@@ -12,7 +12,9 @@ spec.loader.exec_module(sifu)
 
 snap = 98
 subhalo = 323653
+view = 0
 re_kpc = 1.37177
+redshift = 0.035
 basePath = 'path/to/TNG/Illustris/data/'
 
 #######################################################################
@@ -29,8 +31,13 @@ sifu.mk_particle_files(subhalo, snap, basePath, ex=[1,0,0], FOV=19, overwrite=Tr
 # mk_mock_RSS outputs a RSS file called
 rss_file = 'ilust-'+str(snap)+'-'+str(subahlo)+'-0-127.cube_RSS.fits.gz'
 
-sifu.mk_mock_RSS(snap, subhalo, view=0, sp_samp=1., template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz',\
-                template_SSP='libs/MaStar_CB19.slog_1_5.fits.gz', template_gas='libs/templete_gas.fits.gz', fib_n=7)
+sifu.mk_mock_RSS(star_file='/snap'+str(snap)+'_shalo'+str(subhalo)+'_'+str(view)+'_stars.dat',\
+                 gas_file='/snap'+str(snap)+'_shalo'+str(subhalo)+'_'+str(view)+'_gas.dat',\
+                 sp_samp=1., indir='', red_0=redshift,\
+                 rssf='ilust-'+str(snap)+'-'+str(subhalo)+'-'+str(view),\
+                 template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz',\
+                 template_SSP='libs/MaStar_CB19.slog_1_5.fits.gz', \
+                 template_gas='libs/templete_gas.fits', fib_n=7, cpu_count=4)
 
 #######################################################################
 ###### Step 3: Recombine fiber spectrum to create mock data cube ######
