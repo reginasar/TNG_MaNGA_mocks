69b45b335a8107330cbb5e459d207b5c026d479d
reginasar <50836927+reginasar@users.noreply.github.com>
2023-08-14 21:50:06 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,10 @@
+# This example generates the RSS file, together with the cube_val and 3D datacube
+# We use reduced stellar particle and gas cell files (to 10%) called:
+# snap98_shalo323653_stars.dat and snap98_shalo323653_gas.dat (both in the repo)
+# You will need an SSP template in the libs folder, we use:
+# MaStar_CB19.slog_1_5.fits.gz (in the repo)
+# And an additional emission line template from (e.g. produced with cloudy):
+# templete_gas.fits 
 
 #### For Python versions > 3.5 import module as
 import importlib.util
@@ -10,40 +17,45 @@ spec.loader.exec_module(sifu)
 #import imp
 #sifu = imp.load_source('sin_ifu_clean', 'sin_ifu_clean.py')
 
-snap = 98
-subhalo = 323653
-view = 0
-re_kpc = 1.37177
-redshift = 0.035
-basePath = 'path/to/TNG/Illustris/data/'
+snap = 98 # snapshot number
+subhalo = 323653 # subhalo ID within the snapshot
+re_kpc = 1.37177 # Petrossian effective radius (predicted from fit)
+redshift = 0.035 # redshift of snapshot
+fib_n = 7 # largest bundle size
+ns = 3 * fib_n * (fib_n-1) + 1 #total number of fibres in bundle
 
 #######################################################################
 ###### Step 1: Create particle files ##################################
 #######################################################################
 
-sifu.mk_particle_files(subhalo, snap, basePath, ex=[1,0,0], FOV=19, overwrite=True)
+# For testing, you can use the particle files saved in the libs folder,
+# which contains randomly selected 10% of particles and cells of a TNG50 galaxy
+#sifu.mk_particle_files(subhalo, snap, basePath, ex=[1,0,0], FOV=19, overwrite=True, \
+#             outdir='libs')
 
 #######################################################################
 ###### Step 2: Generate row stacked spectrum (RSS) file ###############
 #######################################################################
 
-#For the next step template gas file must be dowloaded separately and saved in libs directory templete_gas.fits.gz
-# mk_mock_RSS outputs a RSS file called
-rss_file = 'ilust-'+str(snap)+'-'+str(subahlo)+'-0-127.cube_RSS.fits.gz'
+# For the next step template gas file must be dowloaded separately and saved in libs directory templete_gas.fits.gz
+# mk_mock_RSS outputs a RSS file
 
-sifu.mk_mock_RSS(star_file='/snap'+str(snap)+'_shalo'+str(subhalo)+'_'+str(view)+'_stars.dat',\
-                 gas_file='/snap'+str(snap)+'_shalo'+str(subhalo)+'_'+str(view)+'_gas.dat',\
-                 sp_samp=1., indir='', red_0=redshift,\
-                 rssf='ilust-'+str(snap)+'-'+str(subhalo)+'-'+str(view),\
-                 template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz',\
-                 template_SSP='libs/MaStar_CB19.slog_1_5.fits.gz', \
-                 template_gas='libs/templete_gas.fits', fib_n=7, cpu_count=4)
+sifu.mk_mock_RSS('snap98_shalo323653_stars.dat', 'snap98_shalo323653_gas.dat', \
+             indir='../', outdir='../', red_0=redshift,\
+             rssf='TNG50-'+str(snap)+'-'+str(subhalo),\
+             template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz',\
+             template_SSP='libs/MaStar_CB19.slog_1_5.fits.gz', \
+             template_gas='libs/templete_gas.fits', fib_n=fib_n, cpu_count=4)
+
 
 #######################################################################
 ###### Step 3: Recombine fiber spectrum to create mock data cube ######
 #######################################################################
 
-sifu.regrid(rss_file, template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz', \
-            n_fib=7, thet=0.0, R_eff=re_kpc, include_gas=False)
+sifu.regrid('TNG50-'+str(snap)+'-'+str(subhalo)+'-'+str(ns)+'.cube_RSS.fits.gz',\
+            outf='TNG50-'+str(snap)+'-'+str(subhalo),
+            dir_r='../', dir_o='../', n_fib=7, thet=0.0, R_eff=re_kpc, \
+            template_SSP_control='libs/MaStar_CB19.slog_1_5.fits.gz', \
+            include_gas=False)
 
 
